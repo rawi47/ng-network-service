e9ed7e9715855178160c9cd7c2b11039612bbf4f
Mejri Bouraoui <mejri.bouraoui@gmail.com>
2019-05-23 12:10:57 +0100

    Fix devices service

--- src/app/device/device.service.ts
+++ src/app/device/device.service.ts
@@ -1,51 +1,40 @@
-import {Http,Response,RequestOptions,Headers,HttpHeaders} from '@angular/http';
-import {HttpHeaders} from '@angular/common/http';
-import { Observable, observable } from "rxjs";
-import { Injectable } from '@angular/core';
-import {Device} from './device';
-import { map } from 'rxjs/operators';
+import {
+  Http,
+  Response,
+  RequestOptions,
+  Headers
+} from '@angular/http';
+import { Observable, observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Device } from "./device";
+import { map } from "rxjs/operators";
 
 @Injectable()
-export class DeviceService{
+export class DeviceService {
+  constructor(private _httpService: Http) {}
 
-    constructor(private _httpService : Http){}
+  token: string;
 
-    token:any;
+  getAuthToken(user: string, password: string, domain: string): void {
+    const url = "https://192.168.100.100:8443/sdn/v2.0/auth";
+    const body = { login: { user: user, password: password, domain: domain } };
+    this._httpService.post(url, body).subscribe((res: Response) => {
+      localStorage.setItem('token', res.json().record.token);
+    });
+  }
 
-    getAllDevices() :Observable<Device[]>{
+  getAllDevices(): Observable<Device[]> {
 
-        
+    this.token = localStorage.getItem('token'),
+    console.log(this.token);
+    let headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + this.token
+    });
+    const options = new RequestOptions({ headers: headers });
 
-        
-        
-        // var url = "https://192.168.100.100:8443/sdn/v2.0/auth";
-        //     let body = {"login":{"user":"sdn","password":"skyline","domain":"sdn"}};
-        //         this._httpService.post(url,body)
-        //         .subscribe((res:Response) => {
-                  
-        //             this.token = res.json().record.token;
-                    
-                    
-                    
-
-        //         });
-        this.token = "a60e1c3930e347caa3e2e9c292270ead;"
-                console.log(this.token);
-                let headers = new Headers({'Content-Type':'application/json',
-                Authorization: 'Bearer ' + this.token});
-        let options = new RequestOptions({headers: headers});
-
-                  return this._httpService.get("https://192.168.100.100:8443/sdn/v2.0/net/links",{
-                    headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.token)
-                  })
-        .pipe(map((
-            responce :Response)=>responce.json()
-            
-            ));
-
-
-                
-                   
-    }
-
+    return this._httpService
+      .get("https://192.168.100.100:8443/sdn/v2.0/net/links", options)
+      .pipe(map((responce: Response) => responce.json()));
+  }
 }
