b4d2b2267e077ee2f2b68d7e65b7be39d91bd114
Mejri Bouraoui <mejri.bouraoui@gmail.com>
2019-05-23 12:12:51 +0100

    Fixes

--- src/app/device/device.service.ts
+++ src/app/device/device.service.ts
@@ -5,9 +5,9 @@ import {
   Headers
 } from '@angular/http';
 import { Observable, observable } from 'rxjs';
-import { Injectable } from "@angular/core";
-import { Device } from "./device";
-import { map } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { Device } from './device';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DeviceService {
@@ -16,7 +16,7 @@ export class DeviceService {
   token: string;
 
   getAuthToken(user: string, password: string, domain: string): void {
-    const url = "https://192.168.100.100:8443/sdn/v2.0/auth";
+    const url = 'https://192.168.100.100:8443/sdn/v2.0/auth';
     const body = { login: { user: user, password: password, domain: domain } };
     this._httpService.post(url, body).subscribe((res: Response) => {
       localStorage.setItem('token', res.json().record.token);
@@ -27,14 +27,14 @@ export class DeviceService {
 
     this.token = localStorage.getItem('token'),
     console.log(this.token);
-    let headers = new Headers({
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + this.token
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + this.token
     });
     const options = new RequestOptions({ headers: headers });
 
     return this._httpService
-      .get("https://192.168.100.100:8443/sdn/v2.0/net/links", options)
+      .get('https://192.168.100.100:8443/sdn/v2.0/net/links', options)
       .pipe(map((responce: Response) => responce.json()));
   }
 }
